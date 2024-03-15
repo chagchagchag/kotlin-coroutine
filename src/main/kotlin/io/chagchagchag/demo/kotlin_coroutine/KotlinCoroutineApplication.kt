0d85b8e6961bb0f6634aef12b5a8ad314ace927c
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-15 14:12:45 +0900

    first status

---
+++ src/main/kotlin/io/chagchagchag/demo/kotlin_coroutine/KotlinCoroutineApplication.kt
@@ -0,0 +1,11 @@
+package io.chagchagchag.demo.kotlin_coroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCoroutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinCoroutineApplication>(*args)
+}
---
+++ src/test/kotlin/io/chagchagchag/demo/kotlin_coroutine/KotlinCoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.chagchagchag.demo.kotlin_coroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
